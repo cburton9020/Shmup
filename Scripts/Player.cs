7ef4d058e76248288fae690f00fbc404af9e25a2
KyokoKo <arumaka90200@gmail.com>
2025-08-25 00:23:18 -0400

    initial commit

---
+++ Scripts/Player.cs
@@ -0,0 +1,89 @@
+using Godot;
+using System;
+
+
+
+
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 2500.0f;
+	public PackedScene projetilePackedScene;
+	private float shootCooldown = 0.1f; // Time between shots in seconds
+	private ulong lastShotTime = 0;
+
+	private bool CanShoot()
+	{
+		return Time.GetTicksMsec() - lastShotTime >= shootCooldown * 1000;
+	}
+
+
+	public override void _Ready()
+	{
+		projetilePackedScene = GD.Load<PackedScene>("res://Scenes/PlayerProjectile1.tscn");
+		
+		// stage = (BaseStage)GetTree().CurrentScene;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+		GD.Print(Position);
+
+
+
+
+
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+
+			if (Input.IsActionPressed("movement_augment"))
+			{
+				velocity.X = direction.X * (Speed / 2);
+
+			}
+			else
+			{
+				velocity.X = direction.X * (Speed);
+			}
+
+
+			if (Input.IsActionPressed("movement_augment"))
+			{
+				velocity.Y = direction.Y * (Speed / 2);
+
+			}
+			else
+			{
+				velocity.Y = direction.Y * (Speed);
+			}
+
+
+		}
+		else
+		{
+			velocity.X = 0;
+			velocity.Y = 0;
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+
+
+
+		if (Input.IsActionPressed("Shoot1") && CanShoot())
+		{
+
+
+			PlayerProjectile1 projectile = projetilePackedScene.Instantiate<PlayerProjectile1>();
+			GetTree().CurrentScene.AddChild(projectile);
+			projectile.Position = new Vector2(GlobalPosition.X, GlobalPosition.Y);
+
+
+			lastShotTime = Time.GetTicksMsec();
+
+		}
+	}
+}
+
---
+++ Scripts/PlayerProjectile1.cs
@@ -0,0 +1,58 @@
+using Godot;
+using System;
+using System.Runtime.CompilerServices;
+
+public partial class PlayerProjectile1 : CharacterBody2D
+{
+	// Called when the node enters the scene tree for the first time.
+
+	float speed = 3000f;
+	Player player;
+	Timer timer;
+	public PlayerProjectile1 projectile;
+	public PackedScene projetilePackedScene;
+	private AnimatedSprite2D _animatedSprite;
+	private Timer lifespanTimer;
+	Vector2 velocity = new Vector2(0, 1);
+
+	public override void _Ready()
+	{
+		projetilePackedScene = GD.Load<PackedScene>("res://Scenes/PlayerProjectile1.tscn");
+		timer = new Timer();
+		player = GetParent().GetNode<Player>("Player");
+		// stage = (BaseStage)GetTree().CurrentScene;
+		_animatedSprite = GetNode<AnimatedSprite2D>("StarSprite");
+		lifespanTimer = new Timer();
+        AddChild(lifespanTimer);
+        lifespanTimer.WaitTime = 3.0f; // 3 seconds
+        lifespanTimer.OneShot = true;
+        lifespanTimer.Timeout += OnLifespanEnded;
+        lifespanTimer.Start();
+	}
+	private void OnLifespanEnded()
+    {
+        QueueFree(); // Remove the projectile
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		velocity.Y = speed * -1;
+
+		_animatedSprite.Play("MedStar");
+
+
+
+
+
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+
+
+	
+
+
+	
+}
